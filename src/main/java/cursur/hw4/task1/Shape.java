7dbe1d4a536afe3b837308d254e2f953022040a2
Bohdan Bodnar <iloveyoulviv@gmail.com>
2019-12-07 21:36:42 +0200

    finish class Shape

--- src/main/java/cursur/hw4/task1/Shape.java
+++ src/main/java/cursur/hw4/task1/Shape.java
@@ -1,4 +1,17 @@
 package cursur.hw4.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shape {
+
+    private List<Vertices> vertices;
+
+    public Shape() {
+        this.vertices = new ArrayList<>();
+    }
+
+    public void addVertices(Vertices vertices) {
+        this.vertices.add(vertices);
+    }
 }
