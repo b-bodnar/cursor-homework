b12811b6c2e05c6c00aadd1a52bc0ec6ec89d06a
Bohdan Bodnar <iloveyoulviv@gmail.com>
2019-12-07 21:37:32 +0200

    add class Vertices

---
+++ src/main/java/cursur/hw4/task1/Vertices.java
@@ -0,0 +1,17 @@
+package cursur.hw4.task1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Vertices {
+
+    private int x;
+    private int y;
+
+    public Vertices(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
