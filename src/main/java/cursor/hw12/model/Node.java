1c14765f46704f15c5e7da060c515cc1a7c6129c
Bohdan Bodnar <iloveyoulviv@gmail.com>
2020-01-26 13:08:06 +0200

    Add class Node

---
+++ src/main/java/cursor/hw12/model/Node.java
@@ -0,0 +1,17 @@
+package cursor.hw12.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Node {
+
+    private int averageSalary;
+    private int frequency;
+
+    public Node(int data) {
+        this.averageSalary = data;
+        this.frequency = 0;
+    }
+}
