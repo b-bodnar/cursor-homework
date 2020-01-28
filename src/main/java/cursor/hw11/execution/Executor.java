9b449ab9d808ee17e440ce80058a2afe7a2e58cd
Bohdan Bodnar <iloveyoulviv@gmail.com>
2020-01-28 09:36:55 +0200

    Fix homework 11 (#3)
    * Homework #11 is done

--- src/main/java/cursor/hw11/execution/Executor.java
+++ src/main/java/cursor/hw11/execution/Executor.java
@@ -8,6 +8,7 @@ public class Executor {
     public static void execute() {
 
         List<Integer[]> holders = new ArrayList<>();
+
         for (int i = 1; true; i++) {
             holders.add(new Integer[10000000]);
         }
