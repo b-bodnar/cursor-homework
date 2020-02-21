c1ba0a7e33b729767e3c7f45af84935ff4856b7f
Bohdan Bodnar <iloveyoulviv@gmail.com>
2020-02-21 12:02:21 +0200

    Homework #14 done. Created test volatile
    * Homework #14 done. Created test volatile
    
    * Change package name
    
    * Removed redundant flag  initialisation

---
+++ src/main/java/cursor/hw14/Main.java
@@ -0,0 +1,10 @@
+package cursor.hw14;
+
+import cursor.hw14.execute.Executor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Executor.execute();
+    }
+}
---
+++ src/main/java/cursor/hw14/execute/Executor.java
@@ -0,0 +1,38 @@
+package cursor.hw14.execute;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Executor {
+
+    private static boolean flag;
+    private static AtomicInteger count = new AtomicInteger();
+
+    public static void execute() {
+
+        Thread counter = incrementCount();
+        Thread stopCounter = stopIncrement();
+
+        counter.start();
+        stopCounter.start();
+    }
+
+    private static Thread stopIncrement() {
+        return new Thread(() -> {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            flag = true;
+            System.out.println("counter make " + count + " operations!");
+        });
+    }
+
+    private static Thread incrementCount() {
+        return new Thread(() -> {
+            while (!flag) {
+                System.out.println(count.incrementAndGet());
+            }
+        });
+    }
+}
